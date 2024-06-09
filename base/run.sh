95fb2e065e161aa3f5cc24bfd4ada17624261228
amaimaya <mariam.shamsali@gmail.com>
2024-06-09 14:47:16 +0200

    fix: run scripts and readme file

--- base/run.sh
+++ base/run.sh
@@ -1,3 +1,4 @@
+git pull
 cd ../../../..
 Scripts/compile-run.py -I -E mascot MPC-EFC/base/base.mpc
 cd Programs/Source/MPC-EFC/base
--- optimized_rabbit/run.sh
+++ optimized_rabbit/run.sh
@@ -1,3 +1,4 @@
+git pull
 cd ../../../..
 Scripts/compile-run.py -I -E mascot MPC-EFC/optimized_rabbit/optimized_rabbit.mpc -F 31
 cd Programs/Source/MPC-EFC/optimized_rabbit
